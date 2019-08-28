06058c29515c83644b7feaa6d8e6cd69d33288fa
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-08-29 00:03:49 +0200

    Add json serialized (w/ context) response support method

--- src/Controller/Api/BaseController.php
+++ src/Controller/Api/BaseController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Api;
 
 use App\Controller\Api\Contracts\BaseControllerContract;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 /**
  * Class BaseController
@@ -12,6 +14,43 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 abstract class BaseController extends AbstractController implements BaseControllerContract
 {
+    /**
+     * @param $data
+     * @param int $status
+     * @param array $headers
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function jsonWithContext($data, $status = Response::HTTP_OK, $headers = [])
+    {
+        return $this->json(
+            $data,
+            $status,
+            $headers,
+            $this->getDefaultContext()
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getDefaultContext()
+    {
+        $dateCallback = function ($innerObject, $outerObject, string $attributeName, string $format = null, array $context = []) {
+            return $innerObject instanceof \DateTime ? $innerObject->format(\DateTime::ISO8601) : '';
+        };
+
+        $defaultContext = [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+                return $object->getId();
+            },
+            AbstractNormalizer::CALLBACKS => [
+                'createdAt' => $dateCallback,
+            ],
+        ];
+
+        return $defaultContext;
+    }
+
     /**
      * @return \Doctrine\Common\Persistence\ObjectRepository
      */
