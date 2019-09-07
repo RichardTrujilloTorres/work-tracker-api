5ac93dcae7bb7da5ce837556f71bfbb936d66408
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-09-07 21:52:42 +0200

    Fix commits number field name on migration

--- src/Migrations/Version20190824084415.php
+++ src/Migrations/Version20190824084415.php
@@ -22,7 +22,7 @@ final class Version20190824084415 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE commit (id INT AUTO_INCREMENT NOT NULL, repository VARCHAR(255) DEFAULT NULL, branch VARCHAR(255) NOT NULL, commits INT NOT NULL, date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE commit (id INT AUTO_INCREMENT NOT NULL, repository VARCHAR(255) DEFAULT NULL, branch VARCHAR(255) NOT NULL, commits_number INT NOT NULL, date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
