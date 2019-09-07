4cc0e3c50c4eb4823d22f9f3d837750f33e98398
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-09-07 22:06:50 +0200

    Remove usage

--- src/Controller/Api/BaseController.php
+++ src/Controller/Api/BaseController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Api;
 use App\Controller\Api\Contracts\BaseControllerContract;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 /**
