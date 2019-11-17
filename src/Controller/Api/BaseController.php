299c6556be0d580c0ed235e6943d0d18657fb847
Richard Trujillo <richard.trujillo.torres@gmail.com>
2019-11-17 06:55:28 +0100

    Remove unused usage

--- src/Controller/Api/BaseController.php
+++ src/Controller/Api/BaseController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Api;
 
 use App\Controller\Api\Contracts\BaseControllerContract;
-use Knp\Component\Pager\Paginator;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
